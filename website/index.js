269f0851fcfc35b841e312e8f906d156aa901086
Yunzhu Chen <ychen6@olin.edu>
2025-04-07 11:31:46 -0400

    update design, set up questions

--- website/index.js
+++ website/index.js
@@ -1,6 +1,32 @@
 function start() {
-    window.location.href = 'question1.html'; 
+    window.location.href = 'driver_type.html'; 
 }
+
+// Driver type
+function commuting() {
+    window.location.href = "question1.html";
+  }
+
+  function errands() {
+    window.location.href = "question1.html";
+  }
+
+  function leisure() {
+    window.location.href = "question1.html";
+  }
+
+  function work() {
+    window.location.href = "question1.html";
+  }
+
+  function other() {
+    window.location.href = "question1.html";
+  }
+
+  function no_car() {
+    window.location.href = "question1.html";
+  }
+
 
 // Question 1
 function q1_option1() {
@@ -22,9 +48,42 @@ function q2_option2() {
 
 // Question 3
 function q3_option1() {
-    window.location.href = 'end_page.html'; 
+    window.location.href = 'question4.html'; 
 }
 
 function q3_option2() {
+    window.location.href = 'question4.html'; 
+}
+
+// Question 4
+function q4_option1() {
+    window.location.href = 'question5.html'; 
+}
+
+function q4_option2() {
+    window.location.href = 'question5.html'; 
+}
+
+// Question 5
+function q5_option1() {
+    window.location.href = 'question6.html'; 
+}
+
+function q5_option2() {
+    window.location.href = 'question6.html'; 
+}
+
+// Question 6
+function q6_option1() {
     window.location.href = 'end_page.html'; 
+}
+
+function q6_option2() {
+    window.location.href = 'end_page.html'; 
+}
+
+function feedback_submit(element) {
+    if (event.key === 'Enter') {
+        console.log(element.value);
+    }
 }
