de6cc8cacdd510629f7a0c30a8ee73fabe5522ba
Yunzhu Chen <ychen6@olin.edu>
2025-04-07 00:29:20 -0400

    start ui design and page interaction

---
+++ website/index.js
@@ -0,0 +1,30 @@
+function start() {
+    window.location.href = 'question1.html'; 
+}
+
+// Question 1
+function q1_option1() {
+    window.location.href = 'question2.html'; 
+}
+
+function q1_option2() {
+    window.location.href = 'question2.html'; 
+}
+
+// Question 2
+function q2_option1() {
+    window.location.href = 'question3.html'; 
+}
+
+function q2_option2() {
+    window.location.href = 'question3.html'; 
+}
+
+// Question 3
+function q3_option1() {
+    window.location.href = 'end_page.html'; 
+}
+
+function q3_option2() {
+    window.location.href = 'end_page.html'; 
+}
