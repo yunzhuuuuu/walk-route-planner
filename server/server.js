ac636d21e7f77fe15d06730c6057bb0f95a2b977
digitalocean local machine <ramzey.burdette2005@gmail.com>
2025-04-08 07:00:28 +0000

    it work

--- server/server.js
+++ server/server.js
@@ -1,40 +1,47 @@
 
 const express = require("express");
-const fs = require("fs");
+const fs = require("node:fs");
 
 const app = express();
 const port = 3586;
 
 app.use(express.json());
 
-db = {"user_0": "hi"};
+let db = {"user_0": "hi"};
 
-fs.readFile("./data.json", 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+async function load_data() {
+	const data = await fs.readFile("./data.json", 'utf8', (err, data) => {
+    		if (err) {
+        		console.error(err);
+       			 return;
+    		}
 
-    db = data;
+    		db = JSON.parse(data);
 
-});
+	});
+}
 
 app.post("/send", (req, res) => {
         const user = req.body;
+	console.log("user = ");
         console.log(user);
         db[`user_${Object.keys(db).length + 1}`] = user;
+	console.log("db = ");
         console.log(db);
+
+	fs.writeFile('./data.json', JSON.stringify(db, null, 2), (err) => {
+		console.error("error", err);
+	});
+
     }
 );
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`HI IM ON PORT ${port}`);
+    await load_data();
 });
 
-process.on('SIGINT', () => {
-    console.log("goodbye!");
-    fs.writeFile('./data.json', JSON.stringify(db), (err) => {
-        console.error("hey...not working uwu", err);
-    });
+process.on('SIGINT', async () => {
+    console.log("goodbye!"); 
     process.exit(0);
 });
